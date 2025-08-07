2fb8b98bdb66c532c89122ad60ac5dbc61eaa3a6
S Harshavardhan <harsha.echo348@gmail.com>
2025-08-08 00:59:05 +0530

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,16 @@
+#Insertion sort
+
+def insertion(arr):
+
+  for i in range(1,len(arr)):
+    key = arr[i]
+    j = i - 1
+    while j >= 0 and arr[j] > key:
+      arr[j+1] = arr[j]
+      j -= 1
+    arr[j+1] = key
+    
+  return arr
+
+arr1 = [7,2,9,3]
+print(insertion(arr1))
